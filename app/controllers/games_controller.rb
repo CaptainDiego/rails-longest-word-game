8c7bcb9248cc7d749262c6ad4a1bf11ee6f5c134
Diego Gilon <diego.gilon@gmail.com>
2018-03-12 15:47:55 +0100

    longest word ruby

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,15 @@
+require "open-uri"
+require "json"
+
 class GamesController < ApplicationController
   def new
     @letters = generate_grid(10)
+    @attempt = params[:word]
   end
 
   def score
+    @letters = params[:letters].split
+    @score = run_game(params[:word], @letters)
   end
 
   private
@@ -15,4 +21,30 @@ class GamesController < ApplicationController
     grid_size.times { grid << alphabet.sample }
     return grid
   end
+
+
+  def run_game(attempt, grid)
+  # TODO: runs the game and return detailed hash of result
+  url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+  user_serialized = open(url).read
+  user = JSON.parse(user_serialized)
+
+  result = {
+    message: ""
+  }
+  if user["found"] == true
+    arr = attempt.upcase.scan(/\w/)
+    if arr.all? { |letter| arr.count(letter) <= grid.count(letter) }
+      result[:message] = "Well done"
+      # result[:score] = (attempt.size * 30) / (result[:time])
+    else
+      result[:message] = "not in the grid"
+      # result[:score] = 0
+    end
+  else
+    result[:message] = "is not an english word"
+    # result[:score] = 0
+  end
+  return "#{attempt} #{result[:message]}"
+end
 end
